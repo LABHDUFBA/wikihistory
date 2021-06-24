4b6f5f86665f55678acff44163ca5fdf667c36ec
Beatriz Milz <beatriz.milz@hotmail.com>
2021-06-23 23:09:02 -0300

    update cleanip

--- R/clean_ip.R
+++ R/clean_ip.R
@@ -9,7 +9,7 @@
 clean_ip <- function(ip_vector) {
 
   remove <- ip_vector %>%
-    stringr::str_detect(pattern =  "\\d\\d\\d\\.|\\d\\d\\d\\d\\:", negate = TRUE)
+    stringr::str_detect(pattern =  "\\d\\d\\d\\.|\\d\\d\\d\\d\\:|\\.\\d\\d\\d|\\.\\d\\d", negate = TRUE)
 
   ip_vector_filtered <- ip_vector[remove]
 
