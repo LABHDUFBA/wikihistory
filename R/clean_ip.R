07e3ce35c552d89e66864f0d9674e0542929d59a
Beatriz Milz <beatriz.milz@hotmail.com>
2021-06-23 23:06:02 -0300

    clean_ip

---
+++ R/clean_ip.R
@@ -0,0 +1,18 @@
+#' Title
+#'
+#' @param ip_vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean_ip <- function(ip_vector) {
+
+  remove <- ip_vector %>%
+    stringr::str_detect(pattern =  "\\d\\d\\d\\.|\\d\\d\\d\\d\\:", negate = TRUE)
+
+  ip_vector_filtered <- ip_vector[remove]
+
+  ip_vector_filtered
+
+}
